93ed3e24ecadf58f23736ce074edef04796b0c6d
Naru412 <narumi.h13427@outlook.com>
2026-07-04 22:33:42 +0900

    feat: 管理者日次勤怠一覧に休憩合計データ表示

--- src/resources/views/admin/attendance_list.blade.php
+++ src/resources/views/admin/attendance_list.blade.php
@@ -29,8 +29,39 @@
             <td>{{ $attendance->user->name }}</td>
             <td>{{ \Carbon\Carbon::parse($attendance->clock_in)->format('H:i') }}</td>
             <td>{{ \Carbon\Carbon::parse($attendance->clock_out)->format('H:i') }}</td>
-            <td></td>
-            <td></td>
+            <td>
+                @php
+                    $breakMinutes = 0;
+
+                    foreach ($attendance->breaks as $break) {
+                        if ($break->break_start && $break->break_end) {
+                            $breakMinutes += \Carbon\Carbon::parse($break->break_start)
+                                ->diffInMinutes(\Carbon\Carbon::parse($break->break_end)); 
+                        }
+                    }
+
+                    $breakHours = floor($breakMinutes /60);
+                    $breakMinute = $breakMinutes % 60;
+                @endphp
+
+                @if ($breakMinutes > 0)
+                    {{ sprintf('%02d:%02d', $breakHours, $breakMinute) }}
+                @endif
+            </td>
+            <td>
+                @if($attendance->clock_in && $attendance->clock_out)
+                    @php
+                        $workMinutes = \Carbon\Carbon::parse($attendance->clock_in)
+                            ->diffInMinutes(\Carbon\Carbon::parse($attendance->clock_out));
+
+                        $totalMinutes = $workMinutes - $breakMinutes;
+                        $hours = floor($totalMinutes / 60);
+                        $minutes = $totalMinutes % 60;
+                    @endphp
+
+                    {{ sprintf('%02d:%02d', $hours, $minutes) }}
+                @endif
+            </td>
             <td>詳細</td>
         </tr>
         @endforeach
